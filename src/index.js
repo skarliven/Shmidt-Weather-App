d58cce6b6e3c0096b970a237d3b9287ff19d5bcc
ishkanderka <victoriashmidt@outlook.com>
2022-09-09 01:27:01 -0600

    Added date and time to js, and updated css + html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Feature #1: Header City Link Display//
+//Feature #1: Highlighted Cities Link Display//
 //New York//
 function getNy(event) {
   event.preventDefault();
@@ -59,6 +59,64 @@ function getNd(event) {
 let newDelhiLink = document.querySelector("#new-delhi");
 newDelhiLink.addEventListener("click", getNd);
 
+// Feature #3: Date & Time //
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let currentDay = days[date.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let currentMonth = months[date.getMonth()];
+
+  let currentDate = date.getDate();
+
+  return `${currentDay}, ${currentMonth} ${currentDate}`;
+}
+
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let amPm = document.querySelector("#am-pm");
+  if (hour >= 12) {
+    amPm.innerHTML = `PM`;
+  } else {
+    amPm.innerHTML = `AM`;
+  }
+  return `${hour}:${minutes}`;
+}
+
 //API//
 function showWeather(response) {
   let currentTemp = document.querySelector("#degrees");
@@ -73,6 +131,10 @@ function showWeather(response) {
   )}°C`;
   let cityName = document.querySelector("#city");
   cityName.innerHTML = response.data.name.toUpperCase();
+  let dateElement = document.querySelector("#todays-date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let todaysTime = document.querySelector("#todays-time");
+  todaysTime.innerHTML = formatTime(response.data.dt * 1000);
 }
 
 function searchCity(city) {
