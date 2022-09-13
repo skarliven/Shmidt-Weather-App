3645ba63ed23cd7e5bb00d9d7da3ad629431322e
ishkanderka <victoriashmidt@outlook.com>
2022-09-12 23:36:52 -0600

    Added forecast structure to JS

--- src/index.js
+++ src/index.js
@@ -107,6 +107,34 @@ function formatTime(timestamp) {
   }
   return `${hour}:${minutes}`;
 }
+//Forecast//
+function showForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` 
+     <div class="col-2">
+      <div class="weather-forecast-day">${day}</div>
+         <img
+          src="http://openweathermap.org/img/wn/04d@2x.png"
+          alt=""
+          width="36"
+         />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max"> 18° </span>
+        <span class="weather-forecast-temperature-min"> 12° </span>
+      </div>
+     </div>
+      `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+showForecast();
 
 //API//
 function showWeather(response) {
